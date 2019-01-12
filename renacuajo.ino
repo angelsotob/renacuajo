67bee5e37bb98ad60ad4ac73f2e73a5e3724378a
Angel Soto <angelsotob@outlook.es>
2019-01-12 19:05:00 +0100

    Introduce linefollower robust mode and buzzer funtionslities with songs

--- renacuajo.ino
+++ renacuajo.ino
@@ -41,6 +41,7 @@
 #define pinLeftIR       2
 #define pinRightIR      3
 #define pinButton       4     // The button is active down
+#define pinBuzzer       13
 
 /* Definition of the values ​​that can take continuous rotation servo,
  that is, the wheels */
@@ -50,12 +51,31 @@
 #define rightWheelForwardValue    180
 #define rightWheelBackwardsValue  0
 
+/* Notas musicales */
+#define DO    261
+#define RE    294
+#define MI    330
+#define FA    349
+#define SOL   392
+#define LA    440
+#define SI    494
+#define DOalto   523
+#define SIb   466
+
 /* A object from Servo class is created for each servo */
 Servo leftWheel;
 Servo rightWheel;
 
 /* Variables */
-int buttonState = 1;
+bool buttonState = true;
+int numTones = 7;
+int tones[ ] = {261, 294, 330, 349, 392, 440, 494};
+
+/* Variables of the line follower */
+int rightIR;
+int leftIR;
+int BLACK = 0;
+int WHITE = 1;
 
 /******************************************************************
  *                     Definition of functions                    *
@@ -70,9 +90,9 @@ void stopWheels() {
 }
 
 void moveForwards() {
-  leftWheel.write(leftWheelForwardValue);
+  leftWheel.write(leftWheelForwardValue); 
   delay(10);
-  
+   
   rightWheel.write(rightWheelForwardValue);
   delay(10);
 }
@@ -101,6 +121,69 @@ void moveRight() {
   delay(10);
 }
 
+void happyBirthday(){
+
+  tone(pinBuzzer, DO);
+  delay(450);
+  noTone(pinBuzzer);
+  tone(pinBuzzer, DO);
+  delay(150);
+  tone(pinBuzzer, RE);
+  delay(600);
+  tone(pinBuzzer, DO);
+  delay(600);
+  tone(pinBuzzer, FA);
+  delay(600);
+  tone(pinBuzzer, MI);
+  delay(1200);
+
+  tone(pinBuzzer, DO);
+  delay(450);
+  noTone(pinBuzzer);
+  tone(pinBuzzer, DO);
+  delay(150);
+  tone(pinBuzzer, RE);
+  delay(600);
+  tone(pinBuzzer, DO);
+  delay(600);
+  tone(pinBuzzer, SOL);
+  delay(600);
+  tone(pinBuzzer, FA);
+  delay(1200);
+
+  tone(pinBuzzer, DO);
+  delay(450);
+  noTone(pinBuzzer);
+  tone(pinBuzzer, DO);
+  delay(150);
+  tone(pinBuzzer, DOalto);
+  delay(600);
+  tone(pinBuzzer, LA);
+  delay(600);
+  tone(pinBuzzer, FA);
+  delay(600);
+  tone(pinBuzzer, MI);
+  delay(600);
+  tone(pinBuzzer, RE);
+  delay(600);
+
+  tone(pinBuzzer, SIb);
+  delay(450);
+  noTone(pinBuzzer);
+  tone(pinBuzzer, SIb);
+  delay(150);
+  tone(pinBuzzer, LA);
+  delay(600);
+  tone(pinBuzzer, FA);
+  delay(600);
+  tone(pinBuzzer, SOL);
+  delay(600);
+  tone(pinBuzzer, FA);
+  delay(1200);
+
+  noTone(pinBuzzer);
+}
+
 
 /******************************************************************
  *                             Setup                              *
@@ -108,10 +191,26 @@ void moveRight() {
  
 void setup() {
 
+  pinMode(pinBuzzer, OUTPUT);
   leftWheel.attach(pinLeftWheel);
   rightWheel.attach(pinRightWheel);
 
   stopWheels();
+
+  /* Buzzer test subroutine
+  delay(5000);
+  for (int i = 0; i < numTones; i++) {
+    tone(pinBuzzer, tones[i]);
+    delay(500);
+    }
+  noTone(pinBuzzer);
+  */
+
+  delay(5000);
+
+  
+
+  happyBirthday();
 }
 
 
@@ -120,38 +219,51 @@ void setup() {
  ******************************************************************/
 
 void loop() {
-
-  buttonState = digitalRead(pinButton);
-
-  if (buttonState == 0) {
   
-  delay(2000);
+    rightIR = digitalRead(pinRightIR);
+    leftIR = digitalRead(pinLeftIR);
 
+    if (buttonState) {
+      buttonState = digitalRead(pinButton);
+    }
+    else{                                           // Main routine. Only executed
+     if (rightIR == BLACK){                         // after pressing the button.
+        leftWheel.write(leftWheelForwardValue);
+        delay(10);
+        if (leftIR == WHITE){
+          rightWheel.write(wheelStopValue);
+          delay(10);
+        }
+        else {
+          rightWheel.write(rightWheelForwardValue);
+          delay(10);
+        }
+        delay(50);
+      }
+      else {
+        moveLeft();
+        delay(50);
+      }
+    }
+    
+    
+
+/* Movement test subroutine
   moveForwards();
   delay(2000);
-
   stopWheels();
-
   delay(1000);
-
   moveRight();
   delay(2000);
-  
   stopWheels();
-
   delay(1000);
-  
   moveBackwards();
   delay(2000);
-
   stopWheels();
-
   delay(1000);
-
   moveLeft();
   delay(2000);
-  
   stopWheels();
-  }
-  
+*/
+
 }
